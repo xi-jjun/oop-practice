129f07a9d6f7817319b6462bed068a4f06024386
xi_jjun <rlawowns97@naver.com>
2022-01-23 19:55:51 +0900

    feat: add AnswerRepository

---
+++ hangman-game/src/main/java/hangman/repository/AnswerRepository.java
@@ -0,0 +1,39 @@
+package hangman.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnswerRepository {
+	private static final AnswerRepository instance = new AnswerRepository();
+	private final String TO_BE_ANSWERED = "_";
+	private List<String> currentTriedAnswers = new ArrayList<>();
+	private String answer;
+
+	private AnswerRepository() {
+
+	}
+
+	public static AnswerRepository getInstance() {
+		return instance;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void save(String playerAnswer) {
+		currentTriedAnswers.add(playerAnswer);
+	}
+
+	public List<String> getCurrentTriedAnswers() {
+		return currentTriedAnswers;
+	}
+
+	public void clear() {
+		currentTriedAnswers.clear();
+	}
+}
