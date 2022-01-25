e014eddc3b7f2f192dc4965bcaad058bada7cc84
xi_jjun <rlawowns97@naver.com>
2022-01-26 00:18:35 +0900

    feat: add application start function

--- hangman-game/src/main/java/hangman/Application.java
+++ hangman-game/src/main/java/hangman/Application.java
@@ -1,7 +1,19 @@
 package hangman;
 
+import hangman.controller.FrontController;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Application {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Test Start");
+		Scanner inputReader = new Scanner(System.in);
+		FrontController controller = new FrontController(inputReader);
+		do {
+			controller.run();
+			System.out.println("Restart? Y/N");
+		} while (inputReader.nextLine().equals("Y"));
+		System.out.println("Game End");
 	}
 }
