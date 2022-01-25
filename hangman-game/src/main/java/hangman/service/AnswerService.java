ea6d490d87b6f3bc6a5a4c6dd2ccbceac2f1f5dd
xi_jjun <rlawowns97@naver.com>
2022-01-26 00:18:06 +0900

    refactor: add isEnglish, isContainedAnswer method and comment 수정

--- hangman-game/src/main/java/hangman/service/AnswerService.java
+++ hangman-game/src/main/java/hangman/service/AnswerService.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class AnswerService {
 	private final AnswerRepository answerRepository = AnswerRepository.getInstance();
 	private final String TO_BE_ANSWERED = "_";
+	private final int MIN_TRY_ANSWER_LENGTH = 1;
 	private String answer;
 
 	public void addPlayerAnswer(String playerAnswer) {
@@ -19,6 +20,9 @@ public class AnswerService {
 		 * proper input : i, a, r, coffee, galaxy...
 		 * improper input : aa, avb, fi, aaaaa, bbbbbbb, DUPLICATED_ANSWER
 		 * ++ 입력 받을 때 공백 모두 제거하자!!
+		 *
+		 * !추가사항!
+		 * 영어가 아닌 입력에 대해서 예외처리를 해줘야 함
 		 */
 
 		checkPlayerAnswer(playerAnswer);
@@ -26,15 +30,24 @@ public class AnswerService {
 	}
 
 	/**
-	 * validate whether input is answer
+	 * validate whether input is contained to answer
 	 */
-	public boolean isAnswer(String input) {
+	public boolean isContainedAnswer(String input) {
 		return answer.contains(input);
 	}
 
 	/**
+	 * @param input
+	 * @return if input is equal to answer, then true. Else return false
+	 */
+	public boolean isAnswer(String input) {
+		return answer.equals(input);
+	}
+
+	/**
 	 * init reset method. controller 에서 맨 처음으로 호출하면 될 것 같은데..?
-	 * @param answer : stage answer
+	 *
+	 * @param answer : each stage answer
 	 */
 	public void reset(String answer) {
 		this.answer = answer;
@@ -43,7 +56,7 @@ public class AnswerService {
 	}
 
 	/**
-	 * _ _ a _ _ return list method
+	 * @return '_ _ a _ _' formatted String
 	 */
 	public String getCurrentAnswerState() {
 		List<String> answerState = new ArrayList<>();
@@ -60,6 +73,7 @@ public class AnswerService {
 
 	/**
 	 * 시도한 정답 리스트를 출력하기 위한 메서드. OutputView 객체에 String 형태로 던질 수 있게 했다.
+	 *
 	 * @return currentTriedAnswer list joined by ", "
 	 */
 	public String getCurrentTriedAnswerList() {
@@ -70,6 +84,7 @@ public class AnswerService {
 	 * 역할
 	 * 1. check duplicate answer when save
 	 * 2. check duplicate character when change triedAnswer to stateAnswer
+	 *
 	 * @param input
 	 * @return if 'input' is in getCurrentTriedAnswers, then return true. else return false
 	 */
@@ -79,14 +94,21 @@ public class AnswerService {
 	}
 
 	private boolean isInvalidAnswerLength(String playerAnswer) {
-		return playerAnswer.length() != answerRepository.getAnswer().length() && playerAnswer.length() != 1;
+		return playerAnswer.length() != answer.length()
+				&& playerAnswer.length() != MIN_TRY_ANSWER_LENGTH;
+	}
+
+	private boolean isEnglish(String input) {
+		return input.chars().allMatch(Character::isLetter);
 	}
 
 	private void checkPlayerAnswer(String playerAnswer) {
 		if (isInvalidAnswerLength(playerAnswer)) {
-			throw new IllegalArgumentException("Invalid Answer Length");
+			throw new IllegalArgumentException(playerAnswer + " is Invalid Answer Length\n");
 		} else if (isMatchedAnswer(playerAnswer)) {
-			throw new IllegalArgumentException("Duplicated Player Answer");
+			throw new IllegalArgumentException(playerAnswer + " is Duplicated Player Answer\n");
+		} else if (!isEnglish(playerAnswer)) {
+			throw new IllegalArgumentException(playerAnswer + " is not english\n");
 		}
 	}
 }
