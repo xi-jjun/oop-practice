041696fd181409856299a724e285d29e936de2ea
xi_jjun <rlawowns97@naver.com>
2022-02-09 23:10:06 +0900

    feat: add constant class for magic number

---
+++ hangman-game/src/main/java/hangman/constants/Constant.java
@@ -0,0 +1,12 @@
+package hangman.constants;
+
+public abstract class Constant {
+	// compare player answer to answer
+	public static final int IS_ANSWER = 0;
+	public static final int IS_EXISTED_CHAR = 1;
+	public static final int IS_NOT_EXISTED_CHAR = 2;
+
+	// getPlayerAnswer
+	public static final int ERROR_GET_ANSWER = -1;
+
+}
