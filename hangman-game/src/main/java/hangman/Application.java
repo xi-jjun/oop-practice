ddbf7b173f8733645e354527c64f0ec9a144a6d9
xi_jjun <rlawowns97@naver.com>
2022-02-09 23:15:38 +0900

    refactor: application MainUI 사용하도록 변경

--- hangman-game/src/main/java/hangman/Application.java
+++ hangman-game/src/main/java/hangman/Application.java
@@ -1,17 +1,17 @@
 package hangman;
 
-import hangman.controller.FrontController;
-
 import java.io.IOException;
 import java.util.Scanner;
+
+import hangman.view.MainUI;
 
 public class Application {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Test Start");
 		Scanner inputReader = new Scanner(System.in);
-		FrontController controller = new FrontController(inputReader);
+		MainUI main = new MainUI();
 		do {
-			controller.run();
+			main.process();
 			System.out.println("Restart? Y/N");
 		} while (inputReader.nextLine().equals("Y"));
 		System.out.println("Game End");
